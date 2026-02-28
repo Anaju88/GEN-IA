63c98e687bf25045132e12c9adeb0f618232cabb
anaju88 <anajoliveira888@gmail.com>
2026-02-27 21:06:29 -0300

    introdução a python

---
+++ PYTHON/tipos_de_dados.py
@@ -0,0 +1,12 @@
+print (11+300+1000) #soma
+print (10-5) #subtração
+print (10*5) #multiplicação
+print (10/5) #divisão
+print(True) #booleano
+print(False) #booleano  
+print("Olá, mundo!") #string
+
+int(10) #inteiro
+str("Olá, mundo!") #string
+float(3.14) #número de ponto flutuante
+bool(True) #booleano
